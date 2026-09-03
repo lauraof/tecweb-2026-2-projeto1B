a574fb9ad18d4e5a12a0acf6fe6d1e25f5613199
Laura de Oliveira <lauradeoli06@gmail.com>
2026-09-03 09:23:47 -0300

    CRUD implementado

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+    path('edit/<int:id>/', views.update, name='edit')
 ]
--- notes/views.py
+++ notes/views.py
@@ -11,3 +11,19 @@ def index(request):
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
+
+def delete(request, id):
+    nota = Note.objects.get(id=id)
+    nota.delete()
+    return redirect('index')
+
+def update(request, id):
+    if request.method == "POST":
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        nota = Note(id=id, title=title, content=content)
+        nota.save()
+        return redirect('index')
+    else:
+        nota = Note.objects.get(id=id)
+        return render(request, 'notes/edit.html', {'note': nota})
