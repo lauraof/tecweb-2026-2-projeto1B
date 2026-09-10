2d97a2b4e8f9fa31b8077cee90df7669b3bd9420
Laura de Oliveira <lauradeoli06@gmail.com>
2026-09-10 14:32:34 -0300

    Tarefa 3 completa

--- getit/settings.py
+++ getit/settings.py
@@ -80,6 +80,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'getit',
+#         'USER': 'getituser',
+#         'PASSWORD': 'getitsenha',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
